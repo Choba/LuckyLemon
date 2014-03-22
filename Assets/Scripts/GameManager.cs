614951a8613e45c69d6274a65d22b48b40e35477
tgraff <t.graff@illuce-entertainment.com>
2014-03-22 18:09:06 +0100

    added new GameManager that handles global stuff (endGame, restart,...) + changed knife collider to match mesh

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+    private static int winningPlayerId = -1;
+
+    public GUIText winText;
+
+    private static float restartTimer;
+
+	// Use this for initialization
+    void Start() {
+        winText.text = "";
+        restartTimer = -1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (winningPlayerId > 0) {
+            winText.text = "Player " + winningPlayerId + " survived!\nPlayer " + (winningPlayerId % 2 + 1) + ", you're lemonade :(";
+        }
+
+        if (restartTimer > -1) {
+            restartTimer += Time.deltaTime;
+
+            if (restartTimer > 2.0f) {
+                RestartLevel();
+            }
+        }
+	}
+
+    public static void EndGame(int winningPlayer)
+    {
+        winningPlayerId = winningPlayer;
+        restartTimer = 0;
+        Debug.Log("player " + winningPlayerId + " won");
+    }
+
+    private void RestartLevel() {
+        Debug.Log("restart");
+        Application.LoadLevel(Application.loadedLevel);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,9 +6,8 @@ public class PlayerController : MonoBehaviour {
     public float moveSpeed;
     public float speedIncreasePerCoin = 0.01f;
 
-	public GUIText pointsText;
-	public GUIText winText;
-    public enum Players { player1, player2 };
+    public GUIText pointsText;
+    public enum Players { player1 = 1, player2 = 2 };
     public Players playerNum;
     public GameObject opponent;
     public int coins;
@@ -16,71 +15,53 @@ public class PlayerController : MonoBehaviour {
     public float stompRadius = .5f;
     public float stompPower = 1;
 
-	private float restartTimer;
+    void Start() {
+        updateHUD();
+    }
 
-	void Start() {
-		restartTimer = -1;
-		updateHUD();
-		winText.text = "";
-	}
-
-	void Update() {
-		if(restartTimer > -1) {
-			restartTimer += Time.deltaTime;
-
-			if(restartTimer > 2.0f) {
-				RestartLevel();
-			}
-		}
-
+    void Update() {
         if (Input.GetButtonDown("Stomp" + (playerNum == Players.player1 ? 1 : 2))) {
             Stomp();
         }
-	}
+    }
 
-	void FixedUpdate() {
-		/*if ((Input.GetKeyDown (KeyCode.Space) || Input.touchCount >= 1) && bCanBoost){
-			//rigidbody.velocity = boostSpeed * rigidbody.velocity.normalized;
-			rigidbody.AddForce(rigidbody.velocity.normalized * boostSpeed, ForceMode.Impulse);
-			bCanBoost = false;
-			boostTimer = 0; //starts the boost timer
-		}*/
-		float moveHorizontal = 0;
+    void FixedUpdate() {
+        /*if ((Input.GetKeyDown (KeyCode.Space) || Input.touchCount >= 1) && bCanBoost){
+            //rigidbody.velocity = boostSpeed * rigidbody.velocity.normalized;
+            rigidbody.AddForce(rigidbody.velocity.normalized * boostSpeed, ForceMode.Impulse);
+            bCanBoost = false;
+            boostTimer = 0; //starts the boost timer
+        }*/
+        float moveHorizontal = 0;
         float moveVertical = 0;
-        
-        if (playerNum == Players.player1)
-        {
-		    moveHorizontal = Input.acceleration.x * 2 + Input.GetAxis ("Horizontal1");
-		    moveVertical = Input.acceleration.y * 2 + Input.GetAxis ("Vertical1");
-        }
-        else
-        {
-		    moveHorizontal = Input.acceleration.x * 2 + Input.GetAxis ("Horizontal2");
-		    moveVertical = Input.acceleration.y * 2 + Input.GetAxis ("Vertical2");
+
+        if (playerNum == Players.player1) {
+            moveHorizontal = Input.acceleration.x * 2 + Input.GetAxis("Horizontal1");
+            moveVertical = Input.acceleration.y * 2 + Input.GetAxis("Vertical1");
+        } else {
+            moveHorizontal = Input.acceleration.x * 2 + Input.GetAxis("Horizontal2");
+            moveVertical = Input.acceleration.y * 2 + Input.GetAxis("Vertical2");
         }
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-		rigidbody.AddForce(movement * moveSpeed * Time.deltaTime);
-        
-	}
+        rigidbody.AddForce(movement * moveSpeed * Time.deltaTime);
+
+    }
 
     void LateUpdate() {
-        if (rigidbody.velocity.magnitude > 1)
-        {
+        if (rigidbody.velocity.magnitude > 1) {
             transform.rotation = Quaternion.LookRotation(rigidbody.velocity, Vector3.up);
         }
     }
 
     void Stomp() {
         Debug.Log("stomp");
-        Vector3 stompPos = transform.position;
         Collider[] objectsInRange = Physics.OverlapSphere(transform.position, stompRadius);
 
         foreach (Collider col in objectsInRange) {
             try {
                 PlayerController enemy = col.transform.parent.GetComponent<PlayerController>();
 
-                if (enemy && enemy.playerNum != playerNum)
-                {
+                if (enemy && enemy.playerNum != playerNum) {
                     /*RaycastHit hit;
                     bool exposed = false;
 
@@ -98,40 +79,33 @@ public class PlayerController : MonoBehaviour {
                     Debug.Log("stomp hit " + enemy + " at dist " + (col.transform.position - transform.position).magnitude);
                     enemy.rigidbody.AddExplosionForce(stompPower, transform.position, stompRadius, .2f);
                 }
-            }
-            catch (NullReferenceException e) {
+            } catch (NullReferenceException e) {
                 /* empty */
             }
         }
     }
 
-	void OnDestroy() {
-		renderer.enabled = false;
-		winText.text = "You're lemonade! :(";
-		restartTimer = 0;
-	}
+    void OnDestroy() {
+        renderer.enabled = false;
+        Debug.Log("player " + playerNum + " destroyed");
+        GameManager.EndGame((int)playerNum % 2 + 1);
+    }
 
-	public void updateHUD() {
-		/*int fruits = GameObject.FindGameObjectsWithTag("Fruit").Length;
-		pointsText.text = "Fruits: " + fruits.ToString();
+    public void updateHUD() {
+        /*int fruits = GameObject.FindGameObjectsWithTag("Fruit").Length;
+        pointsText.text = "Fruits: " + fruits.ToString();
 
-		if(fruits == 0) {
-			winText.text = "YOU WIN!!!";
-			restartTimer = 0;
-		}*/
-	}
+        if(fruits == 0) {
+            winText.text = "YOU WIN!!!";
+            restartTimer = 0;
+        }*/
+    }
 
-	private void RestartLevel() {
-		Application.LoadLevel(Application.loadedLevel);
-	}
-
-    public void getCoins(int amount)
-    {
+    public void getCoins(int amount) {
         coins += amount;
         moveSpeed *= 1.0f + (speedIncreasePerCoin * amount);
     }
-    public void loseCoins(int amount)
-    {
+    public void loseCoins(int amount) {
         coins -= amount;
         moveSpeed /= 1.0f + (speedIncreasePerCoin * amount);
     }
