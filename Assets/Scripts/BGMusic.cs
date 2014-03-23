d081236b4ff97a58d109f14a7796203811b9a173
stfuhrmann <fumse@live.de>
2014-03-23 18:26:40 +0100

    + loop bgm

---
+++ Assets/Scripts/BGMusic.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BGMusic : MonoBehaviour {
+
+    public static BGMusic instance;
+
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
